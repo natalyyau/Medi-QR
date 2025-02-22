582ebce454f297386ff849ec72053ac61285d509
natalyyau <natalyyau1@gmail.com>
2025-02-22 01:31:29 -0500

    refactor: move Results component to pages directory and update import path

--- health-risk/src/App.jsx
+++ health-risk/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Assessment from "./pages/assessment";
-import Results from "./components/results";
+import Results from "./pages/results";
 
 function App() {
   return (
