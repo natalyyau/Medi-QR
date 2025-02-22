649ace71389e6a8bc7ee4eb51672ce91e5f4e34b
SiIentRiver <zhashmi7@gatech.edu>
2025-02-22 04:47:40 -0500

    Dead Branch: Unable to access assessment page

--- health-risk/src/pages/results.jsx
+++ health-risk/src/pages/results.jsx
@@ -1,14 +1,48 @@
+// All Backend - Frontend communication is done IN THIS FILE
 import React, { useEffect, useState } from 'react'
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+  
+//// Send Data to Backend
+const Results = () => {
+  // Retrieves Questionare Answers from Questionairre
+  let location = useLocation();
+  const { answers } = location.state || {};
 
-const [healthStatus, setHealthStatus] = useState([{}])
-
+  // Sends Code to Backend
+  const sendAnswersToBackend = async () => {
+    const response = await axios.post("http://localhost:5000/api/assess", answers);
+   }
+  
+// Send Data if Answers exists
 useEffect(() => {
-  fetch("/api").then(
-    response => response.json()
-  ).then(
-    data => {
-      setHealthStatus(data)
+  if (answers) {
+    sendAnswersToBackend();
     }
-  )
-}, [])
+  }, [answers]);
 
+
+//// Recieves Data from Backend
+const [healthStatus, setHealthStatus] = useState([{}]);
+
+  useEffect(() => {
+    // Using axios to fetch data
+    axios.get("/api")
+      .then((response) => {
+        setHealthStatus(response.data); // setting the state with the response data
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Health Status</h1>
+      {/* Render healthStatus data */}
+      <pre>{JSON.stringify(healthStatus, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default Results;
